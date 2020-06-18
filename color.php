7d818f93a894bd01ff4975187813b03aca0352ae
Jessica <jrcarolyn@gmail.com>
2020-06-18 14:23:53 +0530

    Update color.php
    Changes made by 1st developer

--- color.php
+++ color.php
@@ -7,6 +7,7 @@ $color = "red";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $coLOR . "<br>";
+echo "This is my first project in GIT";
 ?>
 
 </body>
