b6ab83733dc9820c2b46620b2cb12ace3bbfe67c
Jessica <jrcarolyn@gmail.com>
2020-06-18 14:29:08 +0530

    Revert "Update color.php"
    This reverts commit 7d818f93a894bd01ff4975187813b03aca0352ae.

--- color.php
+++ color.php
@@ -7,7 +7,6 @@ $color = "red";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $coLOR . "<br>";
-echo "This is my first project in GIT";
 ?>
 
 </body>
